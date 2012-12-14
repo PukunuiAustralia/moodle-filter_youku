8d47100551ecc48f782c5fa2d3f892ecf550b572
Shane Elliott <shane@pukunui.com>
2012-12-14 09:02:34 +0800

    Add maturity and release to version info

--- version.php
+++ version.php
@@ -11,6 +11,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012112000;     // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012062500;     // Requires this Moodle version
-$plugin->component = 'filter_youku'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2012112000;      // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012062500;      // Requires this Moodle version
+$plugin->component = 'filter_youku';  // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_STABLE; // This version's maturity level
+$plugin->release   = '1.0';           // Release name
